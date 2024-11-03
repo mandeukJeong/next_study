706f7270910543201a61920d79fd5a874dde83dd
mandeukJeong <wjdalstj137@gmail.com>
2024-11-03 21:51:28 +0900

    #3.1 Client Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,28 @@
-export const metadata = {
-    title: 'Home'
-}
+'use client';
+
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//     title: 'Home'
+// };
 
 export default function Home() {
-    return <h1>Home</h1>;
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div>
+            {isLoading ? 'Loading...' : JSON.stringify(movies)}
+        </div>
+    );
 }
